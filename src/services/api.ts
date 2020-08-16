77021e2f6060d0329397b0f6b0339c78d7f07059
Cláudio Silva <claudio.engdist@gmail.com>
2020-08-16 19:10:09 -0300

    Update api.ts
    server url fix http -> https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://proffy-srv.herokuapp.com/',
+    baseURL: 'https://proffy-srv.herokuapp.com/',
 });
 
 export default api;
